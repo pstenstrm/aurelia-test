01b235de76f2191c4dc4a364c9c0b673481d0bf5
Per Stenström <per@vinnovera.se>
2015-02-27 16:01:22 +0100

    jsonp

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,12 +1,24 @@
 import {HttpClient} from 'aurelia-http-client';
 
+var 
+	url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+
 export class Weather {
 	static inject() { return [HttpClient]; }
-	configuration(http) {
+
+	constructor(http) {
 		this.http = http;
+		this.location = 'Paris';
+		this.weather= [];
 	}
 
 	activate() {
-		
+
+	}
+
+	search() {
+		return this.http.jsonp(url + this.location).then(response => {
+			this.weather.push(response);
+		});
 	}
 }
